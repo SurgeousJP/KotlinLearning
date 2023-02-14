e4e9a3df94c574e87e70bd81446898eaca7c1a29
Surgeous <tuanbao2311@gmail.com>
2023-02-14 15:36:19 +0700

    LateInit Property Usage

--- LateInit/src/main/kotlin/Main.kt
+++ LateInit/src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var user = User("SurgeousJP", "Unknown", 20)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Note that if U don't use it, kotlin will throw UninitializedPropertyAccessException
+    user.favoriteMovie = "Interstellar"
+
+    println(user.favoriteMovie)
+}
+
+// LateInit keyword
+// Only work for non-primitive CLASSES (string, array, ...)
+// Sometimes, you don't want to include the property as constructor
+// and don't want to assign its own values directly in class
+// And that's when lateinit come into work !
+
+class User(var firstName: String, var lastName: String, var age: Int){
+    lateinit var favoriteMovie: String
 }
